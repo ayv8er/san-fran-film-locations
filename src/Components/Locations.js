a6a21f50f1c3ab09bc8d057e8db88e7a183ed73b
Richard Oh <n24512@protonmail.com>
2022-04-02 02:15:10 -0500

    renders each movie title in component

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -3,19 +3,26 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Locations = () => {
-  const [locations, setLocations] = useState({});
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
     axios
       .get("https://data.sfgov.org/resource/yitu-d5am.json")
       .then((res) => {
         setLocations(res.data);
+        console.log(res.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  });
-  return null;
+  }, []);
+  return (
+    <>
+      {locations.map((loc, index) => {
+        return <p key={index}>{loc.title}</p>;
+      })}
+    </>
+  );
 };
 
 export default Locations;
