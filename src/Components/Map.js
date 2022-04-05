9d465b71eb0e1b09a2549f14beae2ae4f5dd642f
Richard Oh <n24512@protonmail.com>
2022-04-05 14:29:25 -0500

    router added for itinerary

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,7 +1,15 @@
 import { useEffect, useRef } from "react";
 
 const Map = (props) => {
-  const { dragOver, drop, map, setMap, markers } = props;
+  const {
+    dragOver,
+    drop,
+    map,
+    setMap,
+    markers,
+    googleMarkers,
+    setGoogleMarkers,
+  } = props;
   const ref = useRef(null);
 
   useEffect(() => {
@@ -18,14 +26,22 @@ const Map = (props) => {
   useEffect(() => {
     if (map) {
       markers.map((position, index) => {
-        return new window.google.maps.Marker({
+        const mark = new window.google.maps.Marker({
           position,
           label: `${index + 1}`,
           map,
         });
+        setGoogleMarkers([...googleMarkers, mark]);
+        return mark;
       });
     }
   }, [map, markers]);
+
+  useEffect(() => {
+    if (markers.length === 0 && googleMarkers.length > 0) {
+      setGoogleMarkers([]);
+    }
+  }, [markers, googleMarkers]);
 
   return (
     <div
@@ -36,7 +52,7 @@ const Map = (props) => {
         drop(event);
       }}
     >
-      <div ref={ref} style={{ height: "50vh" }} />;
+      <div ref={ref} style={{ height: "50vh" }} />
     </div>
   );
 };
--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,24 +1,61 @@
+import { NavLink } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
 const Searchbar = (props) => {
-  const { searchTitle, setSearchTitle } = props;
+  const { searchTitle, setSearchTitle, googleMarkers, setMarkers } = props;
 
   const handleChange = (event) => {
     event.preventDefault();
     setSearchTitle(event.target.value);
   };
 
+  const handleClick = (event) => {
+    event.preventDefault();
+    googleMarkers.map((m) => m.setMap(null));
+    setMarkers([]);
+  };
+
   return (
-    <div className="d-flex justify-content-center" style={{ margin: "1%" }}>
-      <label>
-        Search by Movie Title:
-        <input
-          type="text"
-          id="title"
-          name="search"
-          placeholder="enter title here"
-          onChange={handleChange}
-          value={searchTitle}
-        />
-      </label>
+    <div
+      className="d-flex justify-content-around"
+      style={{ width: "100%", margin: "1%" }}
+    >
+      <div className="d-flex justify-content-center" style={{ width: "50%" }}>
+        <label>
+          Search by Movie Title:
+          <input
+            type="text"
+            id="title"
+            name="search"
+            placeholder="enter title here"
+            onChange={handleChange}
+            value={searchTitle}
+          />
+        </label>
+      </div>
+
+      <div className="d-flex justify-content-center" style={{ width: "50%" }}>
+        <Button variant="primary" size="sm" as={NavLink} to="/">
+          Film Locations
+        </Button>
+        <Button
+          style={{ marginLeft: "1%" }}
+          variant="primary"
+          size="sm"
+          as={NavLink}
+          to="/itinerary"
+        >
+          Itinerary
+        </Button>
+        <Button
+          style={{ marginLeft: "1%" }}
+          variant="danger"
+          size="sm"
+          onClick={handleClick}
+        >
+          Remove Markers
+        </Button>
+      </div>
     </div>
   );
 };
